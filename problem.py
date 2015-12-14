f9a234f3a210c4c70f6cf9f6293c4b06a123445d
anhpt204 <anh.pt204@gmail.com>
2015-12-14 23:57:43 +0700

    gmctp problem

--- problem.py
+++ problem.py
@@ -474,7 +474,67 @@ class MCTPProblem(CTPProblem):
                 if xs[i+1] == 1:
                     self.nodes[xs[0]].cover_list.append(i)
                     self.nodes_covering_customer[i].update([xs[0]])
+                    
+def GMCTPProblem(CTPProblem):
+    def load_data(self, data_path):
+        self.data_path = data_path
+        self.name = basename(data_path)
         
+        lines = open(data_path, 'r').readlines()
+        
+        xs = [x for x in lines[0].split()]
+        self.num_of_nodes = int(xs[0])
+        self.num_of_customers = int(xs[1])
+        obligatory_node = int(xs[2])
+        
+        self.max_nodes_per_route=int(xs[3])
+        self.max_tour_length = float(xs[4])
+        
+        
+        if len(xs)>5:
+            self.best_cost = float(xs[5])
+        else:
+            self.best_cost = 0
+        
+        self.obligatory_nodes = set(range(1,obligatory_node))
+        
+        # init    
+        for l in xrange(self.num_of_customers):
+            self.nodes_covering_customer[l]=set()
+
+        # initialize nodes
+        self.nodes = []
+        for i in xrange(self.num_of_nodes + obligatory_node):
+            node = CTPNode(id=i, visited_cost=0, coverage_demand=0)
+            self.nodes.append(node)
+            
+        # load cost matrix
+        i = 1
+        line = lines[i]
+        while not line.isspace():
+            xs = line.split()
+            id1, id2 = [int(x) for x in xs[:2]]
+            distance = float(xs[-1])
+            self.nodes[id1].cost_dict[id2] = distance
+            self.nodes[id2].cost_dict[id1] = distance
+            
+            i += 1
+            line = lines[i]
+        
+        # load covering matrix
+        i += 1
+        for line in lines[i:-1]:
+            xs = [int(x) for x in line.split()]
+            for i in xrange(self.num_of_customers):
+                if xs[i+1] == 1:
+                    self.nodes[xs[0]].cover_list.append(i)
+                    self.nodes_covering_customer[i].update([xs[0]])
+        
+        
+        # load customer covering constraint
+        self.customer_covering = [int(x) for x in lines[-1].split()]
+        
+            
 if __name__ == '__main__':
     data_path = '/home/pta/projects/ctp/data_ctp/kroA-13-12-75-1.ctp'
     problem = CTPProblem()
