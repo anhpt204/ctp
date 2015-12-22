f0bef3c1ce07b79d5d37d9c1f4161c0d191e99c4
anhpt204 <anh.pt204@gmail.com>
2015-12-22 12:47:19 +0700

    data for running on vast

---
+++ rename4vast.py
@@ -0,0 +1,16 @@
+'''
+Created on Dec 22, 2015
+
+@author: anhpt4
+'''
+from os.path import join
+import glob 
+
+dir_input = 'data_gmctp'
+dir_output = 'gmctp_vast_input'
+
+files = glob.glob(join(dir_input, "*.ctp"))
+for i, file in zip(xrange(1, len(files)+1), files):
+    output_file = 'input.'+str(i)
+    txt = open(file).read()
+    open(join(dir_output, output_file),'w').write(txt)
